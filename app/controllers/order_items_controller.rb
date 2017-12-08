650478539e846d26675da7b9236dfdad3c80c588
Margaret Berry <codeberry1@gmail.com>
2017-12-08 14:20:50 -0800

    clear deleted items from cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -7,6 +7,7 @@ class OrderItemsController < ApplicationController
     session[:order_id] = @order.id
     respond_to do |format|
       format.html { redirect_to products_path }
+      format.js
     end
   end
 
@@ -22,7 +23,10 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.find(params[:id])
     @item.destroy
     @order.save
-    redirect_to cart_path
+    respond_to do |format|
+      format.html { redirect_to cart_path }
+      format.js { render 'carts/destroy' }
+    end
   end
 
   private
