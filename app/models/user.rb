8c1bfd934c25e857cb9dbe7a741a1b551f1d6c53
Margaret Berry <codeberry1@gmail.com>
2017-12-11 23:12:28 -0800

    set up specs, add user validations and  write user unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ApplicationRecord
   has_secure_password
   has_many :orders
 
+  validates :email, :presence => true
+  validates :email, :uniqueness => true
+
+  validates :name, :presence => true
+
+  validates :password, :presence => true
+
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
   end
