e1e3280ef580f99b3086d0b8f669ae9696486efe
Margaret Berry <codeberry1@gmail.com>
2017-12-13 17:27:35 -0800

    add ability to update product for admins

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
